20003573ebda466ab8c419888d63cef8aaa9d2f5
HANOK JOSHUA <ugs23176_it.hanok@cbit.org.in>
2026-09-22 12:24:23 +0530

    Time: 82 ms (18.69%), Space: 24.2 MB (25.89%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.py
@@ -0,0 +1,15 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        span = 1
+
+        while self.stack and self.stack[-1][0] <= price:
+            previous_price, previous_span = self.stack.pop()
+            span += previous_span
+
+        self.stack.append((price, span))
+
+        return span
