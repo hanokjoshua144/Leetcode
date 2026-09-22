77130bf3e985c7769fa931f0a747cf7bb164e8b4
HANOK JOSHUA <ugs23176_it.hanok@cbit.org.in>
2026-09-22 13:26:45 +0530

    Time: 64 ms (5.86%), Space: 21.1 MB (23.89%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> list[int]:
+        if not root:
+            return []
+
+        stack = [root]
+        result = []
+
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+
+            for child in reversed(node.children):
+                stack.append(child)
+
+        return result
+
+        for child in reversed(node.children):
+            stack.append(child)
+
+        return result
